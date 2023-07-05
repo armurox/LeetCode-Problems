ade8f8dc9543a91ff203fcf593015a6f87bbde0d
Arman Jasuja <arman_jasuja@yahoo.com>
2023-07-05 14:57:30 +0400

    Comments added to solution

--- easy/best_time_to_buy_and_sell_stock/best.py
+++ easy/best_time_to_buy_and_sell_stock/best.py
@@ -1,13 +1,16 @@
 from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # Method: Split the array in half at minimum price, and calculate maximum profit. Repeat until cannot split anymore.
         max_prof = 0
         min_price = 0
         max_price = 1
+        # The array that will be split in half
         curr_prices = prices
         while curr_prices:
             min_price = min(curr_prices)
             max_price = max(curr_prices[curr_prices.index(min_price):])
+            # Max prof for current minimum value calculated
             curr_prof = max_price - min_price
             if curr_prof > max_prof:
                 max_prof = curr_prof
